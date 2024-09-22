0d7c198e7b3e9db6dcf04976d14da8ec154541ff
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-09-22 15:27:26 +0900

    feat: floating action button 추가

--- part3_daangn/lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
+++ part3_daangn/lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
@@ -1,32 +1,39 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FloatingButtonState {
-  bool isExpanded;
-  bool isSmall;
-
-  FloatingButtonState()
-      : isExpanded = false,
-        isSmall = false;
-}
+import 'w_floating_daangn_button.state.dart';
 
 final floatingButtonStateProvider =
     StateNotifierProvider<FloatingButtonStateNotifier, FloatingButtonState>(
   (ref) => FloatingButtonStateNotifier(
-    FloatingButtonState(),
+    const FloatingButtonState(false, false),
   ),
 );
 
 class FloatingButtonStateNotifier extends StateNotifier<FloatingButtonState> {
   FloatingButtonStateNotifier(super.state);
 
+  bool needButtonBigger = false;
+
   void onTapButton() {
-    state = state
-        ..isExpanded = !state.isExpanded
-        ..isSmall = true;
+    final isExpanded = state.isExpanded;
+    final isSmall = state.isSmall;
+
+    state = state.copyWith(isExpanded: !state.isExpanded, isSmall: needButtonBigger ? false : true);
+
+    if (needButtonBigger) {
+      needButtonBigger = false;
+    }
+
+    if (!isSmall && !isExpanded) {
+      needButtonBigger = true;
+    }
   }
 
   void changeButtonSize(bool isSmall) {
-    state = state
-      ..isSmall = isSmall;
+    if (state.isExpanded) {
+      return;
+    }
+
+    state = state.copyWith(isSmall: isSmall);
   }
 }
