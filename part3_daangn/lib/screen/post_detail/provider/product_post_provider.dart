699a10727f4f1c0ed55400c0e372ce1f2bd039e6
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-10-10 17:49:55 +0900

    feat: riverpod AutoDisposeRef extension으로 캐시 생성

--- part3_daangn/lib/screen/post_detail/provider/product_post_provider.dart
+++ part3_daangn/lib/screen/post_detail/provider/product_post_provider.dart
@@ -1,8 +1,11 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/dart/extension/ref_extension.dart';
 import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../data/network/daangn_api.dart';
 
 final productPostProvider = AutoDisposeFutureProviderFamily<ProductPost, int>((ref, id) async {
+  ref.cacheFor(15.seconds);
   return await DaangnApi.getPost(id);
 });
