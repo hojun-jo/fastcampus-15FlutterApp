230123b2912d53aab7b99e4b2f98f0a340668805
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-10-24 03:05:34 +0900

    feat: realtime database

--- part5_firebase_basic/lib/main.dart
+++ part5_firebase_basic/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
@@ -129,14 +130,44 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: Text("데이터 쓰기"),
             ),
-            ElevatedButton(onPressed: () async {
-              final snapshot = await FirebaseFirestore.instance
-                  .collection("test")
-                  .get();
-              for (var element in snapshot.docs) {
-                print(element.data());
-              }
-            }, child: Text("데이터 읽기"),),
+            ElevatedButton(
+              onPressed: () async {
+                final snapshot =
+                    await FirebaseFirestore.instance.collection("test").get();
+                for (var element in snapshot.docs) {
+                  print(element.data());
+                }
+              },
+              child: Text("데이터 읽기"),
+            ),
+            Divider(),
+            ElevatedButton(
+              onPressed: () async {
+                // await FirebaseDatabase.instance.ref().set({
+                //   "name" : "foo",
+                //   "nickname": "bar",
+                //   "age": 21,
+                // });
+
+                // await FirebaseDatabase.instance.ref().update({
+                //   "name" : "foo",
+                //   "nickname": "bar",
+                //   "age": 22,
+                // });
+
+                await FirebaseDatabase.instance.ref().push().set({
+                  "count": 10,
+                });
+              },
+              child: Text("Realtime Database 데이터 쓰기"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final data = await FirebaseDatabase.instance.ref().get();
+                print(data.value);
+              },
+              child: Text("Realtime Database 읽기"),
+            ),
           ],
         ),
       ),
